f285708220e0321fce770e862eded5cd27f74748
Ana Beatriz <anabiawalker@gmail.com>
2022-01-13 13:29:37 -0300

    Corrigindo URI do Mongo

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-const url = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb//localhost/mymoney'
+const url = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb+srv://<ana>:<comandsharp357A951>@cluster0.xmzzc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 module.exports = mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
